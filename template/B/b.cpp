0f06de0268f849cbb521a40ad72ea691276c245c
Ray_desktop <mail@mail>
2023-02-23 16:46:54 +0900

    CODE_FESTIVAL_2016_qualB

--- template/B/b.cpp
+++ template/B/b.cpp
@@ -1,12 +1,43 @@
+// #include <iostream>
 #include <bits/stdc++.h>
 
 using namespace std;
 using ll = long long;
 
+int main()
+{
+    int N, A, B;
+    string S;
+    cin >> N >> A >> B >> S;
 
-int main(){
+    int rank = 0;
+    int abroadRank = 0;
 
-    cout <<  << endl; 
-
+    for (int i = 0; i < N; ++i)
+    {
+        if (S[i] == 'a')
+        {
+            if (rank < A + B){
+                cout << "Yes" << endl, 
+                ++rank;
+            } else {
+                cout << "No" << endl;
+            }
+        }
+        else if (S[i] == 'b')
+        {
+            if ( rank < A + B && abroadRank < B){
+                cout << "Yes" << endl;
+                ++abroadRank;
+                ++rank;
+            }
+            else
+                cout << "No" << endl;
+        }
+        else
+        {
+            cout << "No" << endl;
+        }
+    }
     return 0;
 }
