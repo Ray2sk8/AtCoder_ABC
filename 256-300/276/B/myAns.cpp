f364fc810655dbdda66a7974a8ed0fcbd5c98f88
Ray_desktop <mail@mail>
2023-02-23 22:35:11 +0900

    276 B by vector

--- 256-300/276/B/myAns.cpp
+++ 256-300/276/B/myAns.cpp
@@ -6,28 +6,33 @@ using namespace std;
 int main()
 {
 
-    int N, K;
+    int N, K;//都市と道の数
 
     cin >> N >> K;
     vector<int> vec[N];
+    
     for (int i = 0; i < K; i++)
     {
         int x, y;
         cin >> x >> y;
-        vec[x - 1].push_back(x);
-        vec[y - 1].push_back(y);
+        vec[x - 1].push_back(y);
+        vec[y - 1].push_back(x);
     }
+    //cout << endl;
 
-    for (int i = 0; i < K; i++)
+    for (int i = 0; i < N; i++)
     {
         cout << vec[i].size() << " ";
         sort(vec[i].begin(), vec[i].end());
+
         for (int j = 0; j < vec[i].size(); j++)
         {
             cout << vec[i][j] << ' ';
         }
         cout << endl;
     }
+
+
 
     return 0;
 }
