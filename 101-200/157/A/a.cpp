cf6032d6cf9ed809ff99c03ad5940bcdf29d9379
Ray2sk8 <winterskyfall369@gmail.com>
2023-02-28 23:04:10 +0900

    157 A

--- 101-200/157/A/a.cpp
+++ 101-200/157/A/a.cpp
@@ -6,7 +6,15 @@ using ll = long long;
 
 int main(){
 
-    cout <<  << endl; 
+    int N;
+    cin >> N;
+
+    if(N%2 == 0){
+    N /= 2;
+    } else {
+    N = N/2 + 1;    
+    }
+    cout << N << endl; 
 
     return 0;
 }
