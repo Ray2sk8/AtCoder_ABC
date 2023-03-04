926a07cceaf7886414e62b6d1929267b9fd011be
Ray2sk8 <winterskyfall369@gmail.com>
2023-03-04 16:34:51 +0900

    156 A:beginer

---
+++ 101-200/156/A/a.cpp
@@ -0,0 +1,25 @@
+#include  <iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+	int N,R;	cin >> N >> R;
+
+	
+
+	if(N < 10){
+
+	    R = R + 100 *(10 - N);
+
+	} 
+
+	
+
+	cout << R ;
+
+	
+
+}
