61e17573ed3aa76713884235b76f3cb6c0ee18b6
Ray2sk8 <winterskyfall369@gmail.com>
2023-03-13 21:10:21 +0900

    247 B

---
+++ 201-255/247/B/b.cpp
@@ -0,0 +1,46 @@
+// #include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    //性と名の入力
+    vector<string> s(N+1);
+    vector<string> t(N+1);
+
+    for(int i=1; i<=N; i++){
+        cin >> s[i] >> t[i];
+    }
+
+    //bool flg_nickName = false;
+    for(int i=1; i<=N; i++){
+        int sCount = 0, tCount = 0;
+        for(int j=1; j<=N; j++){
+            if(i == j){//自分の名前はパス
+                continue;
+            }else{
+                if(s[i] == t[j] || s[i] == s[j]){//性と名前が一致、性が同じ
+                    sCount++;
+                }
+                if(t[j] == s[i] || t[j] == t[i]){//名前と性が一致、名前が同じ
+                    tCount++;
+                }
+            }
+            if(sCount && tCount){
+                cout << "No" << endl;
+                return 0;
+            } 
+        }
+
+
+
+
+    }
+    cout << "Yes" << endl;
+    return 0;
+}
