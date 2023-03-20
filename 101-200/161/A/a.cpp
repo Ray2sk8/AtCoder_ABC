67e5f25a2ab80208c7a8f853e0047a08136b2184
Ray2sk8 <winterskyfall369@gmail.com>
2023-03-20 23:20:10 +0900

    161 A

---
+++ 101-200/161/A/a.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+
+int main(){
+    ll a, b, c, d;
+    cin >> a >> b >> c;
+    swap(a,b);
+    swap(a,c);
+
+    cout << a << " " << b << " "  << c << endl; 
+
+    return 0;
+}
---
+++ 101-200/161/A/a1.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+
+int main(){
+    ll a, b, c;
+    cin >> a >> b >> c;
+    // swap(a,b);
+    // swap(a,c);
+
+    cout << c << " " << a << " "  << b << endl; 
+
+    return 0;
+}
