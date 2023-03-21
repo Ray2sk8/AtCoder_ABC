ecc926d66182886d54d252dfe4d6aaf1a0f4ccde
Ray2sk8 <winterskyfall369@gmail.com>
2023-03-21 23:57:08 +0900

    142 B

--- 101-200/142/B/b.cpp
+++ 101-200/142/B/b.cpp
@@ -4,8 +4,19 @@
 using namespace std;
 using ll = long long;
 
-int main()
-{
+int main() {
+    ll N, K, count = 0;
+    cin >> N >> K;
+
+    for (int i = 0; i < N; i++) {
+        ll x;
+        cin >> x;
+        if(K<=x){
+            count++;
+        }
+    }
+
+    cout << count << endl;
 
     return 0;
 }
