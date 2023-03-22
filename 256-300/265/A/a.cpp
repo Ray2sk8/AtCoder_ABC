6cf1eec8a2c0f80fde65f300205a71a3cdc78b38
Ray2sk8 <winterskyfall369@gmail.com>
2023-03-22 13:47:38 +0900

    265 A

--- 256-300/265/A/a.cpp
+++ 256-300/265/A/a.cpp
@@ -5,8 +5,24 @@ using ll = long long;
 
 
 int main(){
+    int x,y,n;
+    int ans = 0;
+    cin >> x >> y >> n;
+    if(n < 3){
+        ans = n*x;
+        cout << ans << endl;
+        return 0;
+    }
 
-    cout <<  << endl; 
+    //ばら売りのほうがやすいとき
+    if( x*3 <= y ){
+        ans = n*x;
+    } else {
+        ans = n/3*y + n%3*x;
+    }
+
+
+    cout << ans << endl; 
 
     return 0;
 }
