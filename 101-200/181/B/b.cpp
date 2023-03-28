d40562e44c6dd81cf1751215d8720646c00fb91d
Ray2sk8 <winterskyfall369@gmail.com>
2023-03-28 21:49:45 +0900

    181 B edit

--- 101-200/181/B/b.cpp
+++ 101-200/181/B/b.cpp
@@ -1,10 +1,27 @@
+/**
+ * @file b.cpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-03-28
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
 // #include <iostream>
 #include <bits/stdc++.h>
 
 using namespace std;
 using ll = long long;
 
-long long fsum(ll a, ll b) {
+/**
+ * @brief 
+ * 
+ * @param a 
+ * @param b 
+ * @return long long 
+ */
+long long fSum(ll a, ll b) {
     ll ret;
 
     ret = (a + b) * (b - a + 1) / 2;
@@ -20,11 +37,15 @@ int main() {
     for (auto &[a, b] : p) {
         cin >> a >> b;
     }
+    // for (auto [a, b] : p) {
+    //     cout<< "a:" << a << " b:" << b  << endl;
+    // }
 
     ll sum = 0;
-    for (auto &[a, b] : p) {
-        sum += fsum(a,b);
+    for (auto [a, b] : p) {
+        sum += fSum(a, b);
     }
     cout << sum << endl;
     return 0;
 }
+
