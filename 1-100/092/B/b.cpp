78f87273e5da0909c06b7603d5a6aceb7ac1f225
Ray2sk8 <winterskyfall369@gmail.com>
2023-03-29 02:05:51 +0900

    092 B, only sample OK...

---
+++ 1-100/092/B/b.cpp
@@ -0,0 +1,46 @@
+// #include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+int N;
+vector<int> A(N + 1);
+int D, X;
+
+// int aiNum(int id) {
+//     int ret = 0;
+
+//     // for(int i=1; (i-1)*A[id]+1<=D;i++){
+//     //     ret++;
+//     // }
+//     int n = (D - 1) / A[id] + 1;
+//     ret = n;
+//     // ret = n*(n+1)*A[id]/2+(1-A[id] )*n;
+
+//     // cout << "id:"<< id << " A:" << A[id] << " n:" << n << " ret:" << ret <<
+//     // endl;
+
+//     return ret;
+// }
+
+int main() {
+
+    cin >> N;
+
+    cin >> D >> X;
+
+    for (int i = 1; i <= N; i++) {
+        cin >> A[i];
+    }
+
+    ll sum = 0;
+    for (int i = 1; i <= N; i++) {
+        // sum += aiNum(i);
+            int n = (D - 1) / A[i] + 1;
+            sum += n;
+    }
+
+    cout << sum + X << endl;
+    return 0;
+}
