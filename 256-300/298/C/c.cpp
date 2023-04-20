72f9bfca4de43b6559af12151231f8a767453500
Ray2sk8 <winterskyfall369@gmail.com>
2023-04-20 21:42:09 +0900

    298 A,B

---
+++ 256-300/298/C/c.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+ll N;
+vector<int>
+
+    int main() {
+
+    int Q;
+    cin >> Q;
+    for (int i = 0; i < Q; i++) {
+        int q;
+        int a, b;
+        cin >> q;
+        if (q == 1) {
+            cin >> a >> b;
+        } else if (q == 2) {
+            askBox();
+        } else if (q == 3) {
+            askCard();
+        }
+    }
+
+    
+
+    return 0;
+}
