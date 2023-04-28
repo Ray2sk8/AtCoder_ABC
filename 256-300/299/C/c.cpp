e74afc6375e1755205cf954a2d1f7a8dc75d1036
Ray2sk8 <winterskyfall369@gmail.com>
2023-04-28 16:59:25 +0900

    init 0428

--- 256-300/299/C/c.cpp
+++ 256-300/299/C/c.cpp
@@ -13,15 +13,14 @@ int main() {
     bool ball = false;
     int level = 0;
     int ans = -1;
-    int ball_start;
-    int ball_end;
 
-    for (int i = 0; i <= N; i++) {
+    N = s.size();
+    for (int i = 0; i < N; i++) {
         switch (s[i]) {
         case 'o':
             if (ball == false) {
-                // 初めてボール
-                ball_start = i;
+                // ボール start
+
                 ball = true;
                 level++;
             } else if (ball == true) {
@@ -29,17 +28,18 @@ int main() {
                 level++;
             }
             break;
+
         case '-':
             if (ball == true) {
-                ball_end = i;
+                //くしざき
+
                 ball = false;
-                if(ball_end - ball_start == level){
-                    cout << "bingo:" << level << endl;
-                }
-                // くしざき
-                if (level > ans) {
-                    ans = level;
-                }
+                // if(ball_end - ball_start == level){
+                //     // cout << "bingo:" << level << endl;
+                // }
+
+                // 最大値更新 
+                ans = max(level, ans);
             }
             level = 0;
             break;
