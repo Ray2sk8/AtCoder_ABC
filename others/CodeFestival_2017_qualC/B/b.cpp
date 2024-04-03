2594b27c7a626e9e3129fb0421baab03935a84fa
Ray2sk8 <winterskyfall369@gmail.com>
2024-04-03 17:52:37 +0900

    CodeFestival2017 C

--- others/CodeFestival_2017_qualC/B/b.cpp
+++ others/CodeFestival_2017_qualC/B/b.cpp
@@ -4,8 +4,42 @@
 using namespace std;
 using ll = long long;
 
-int main()
-{
+ll threepower(int x) {
+    ll ret = 1;
+
+    for (int i = 1; i <= x; i++) {
+        ret *= 3;
+    }
+
+    return ret;
+}
+
+int main() {
+
+    int N;
+    cin >> N;
+    int A[N];
+    int B[N];
+    for (int i = 1; i <= N; i++) {
+        cin >> A[i];
+        if (A[i] % 2 == 0) {
+            B[i] = 2;
+        } else {
+            B[i] = 1;
+        }
+    }
+
+    ll ans;
+    ans = threepower(N);
+
+    ll dec = 1;
+    for (int i = 1; i <= N; i++) {
+        dec *= B[i];
+    }
+
+    ans -= dec;
+
+    cout << ans << endl;
 
     return 0;
 }
